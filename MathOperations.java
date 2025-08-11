b297b2905dd7c9246e008d41b4ec64aea25fd4a2
Tejas Ghondage <130488580+Tejasghondage@users.noreply.github.com>
2025-08-11 22:56:59 +0530

    Static POC-1
    Create a Java program that illustrates the use of a `static` method for performing a basic
    mathematical operation

---
+++ MathOperations.java
@@ -0,0 +1,11 @@
+package statics.mathOperation;
+
+public class MathOperations {
+static void add(int x ,int y) {
+	System.out.println("Sum of "+x+" and "+y+" is :"+(x+y));
+}
+	public static void main(String[] args) {
+//		MathOperations m = new MathOperations();
+		MathOperations.add(12,13);
+	}
+}
